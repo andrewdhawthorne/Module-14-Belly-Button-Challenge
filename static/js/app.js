d46956dc42ae8c476c57fd5d8838708d578e0c4d
andrewdhawthorne <adhawthorne@gmail.com>
2023-08-12 16:07:09 -0500

    setting up dropdown menu

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,50 @@
+// Place url in constant variable 
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-// Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
-
-// Fetch the JSON data and console log it
+// Fetch the JSON data and log it
 d3.json(url).then(function(data) {
     console.log(data);
   });
+
+// Initialize dashboard 
+function init() {
+
+  // Use D3 to select dropdown menu
+  let dropdownMenu = d3.select("#selDataset"); 
+
+  // Use D3 to get sample names for dropdown menu
+  d3.json(url).then((data) => {
+
+    // Assign the value of the dropdown menu option to a variable
+    let names = data.names; 
+
+    // Add samples to dropdown menu 
+    names.forEach((id) => {
+
+      // Log the values of each id 
+      console.log(id); 
+
+      // Append each value to each id 
+      dropdownMenu.append("option")
+      .text(id)
+      .property("value", id); 
+    }); 
+
+    // Pull first sample from list 
+    let first_sample = names[0]; 
+
+    // Log the 
+    console.log(first_sample); 
+
+   // Build the initial plots
+   buildMetadata(first_sample);
+   buildhBarChart(first_sample);
+   buildBubbleChart(first_sample);
+   buildGaugeChart(first_sample);
+  
+  }); 
+
+}; 
+
+init(); 
+
