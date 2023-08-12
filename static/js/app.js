eac663b250f6e6142443912e513c1c6a94f4721a
andrewdhawthorne <adhawthorne@gmail.com>
2023-08-12 16:26:35 -0500

    pulling metadata

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // Place url in constant variable 
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-// Fetch the JSON data and log it
+// Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
     console.log(data);
   });
@@ -18,33 +18,54 @@ function init() {
     // Assign the value of the dropdown menu option to a variable
     let names = data.names; 
 
-    // Add samples to dropdown menu 
+    // Iterate through the names array 
     names.forEach((id) => {
 
-      // Log the values of each id 
+      // Console log the values of the ids  
       console.log(id); 
 
-      // Append each value to each id 
-      dropdownMenu.append("option")
-      .text(id)
-      .property("value", id); 
+      // Append each value to each id and add as an option to the dropdown menu 
+      dropdownMenu.append("option").text(id).property("value", id); 
     }); 
 
     // Pull first sample from list 
     let first_sample = names[0]; 
 
-    // Log the 
+    // Console log the first sample 
     console.log(first_sample); 
 
-   // Build the initial plots
-   buildMetadata(first_sample);
-   buildhBarChart(first_sample);
-   buildBubbleChart(first_sample);
-   buildGaugeChart(first_sample);
+   // Call the functions to create the demographic panel, the bar chart, the bubble chart, and the washing gauge 
+   Metadata(first_sample);
+   hBarChart(first_sample);
+   BubbleChart(first_sample);
+   GaugeChart(first_sample);
   
   }); 
 
 }; 
+
+// Create the demographic information display using the sample metadata 
+function Metadata(selectedValue) {
+
+  // Use D3 to pull all data 
+  d3.json(url).then((data) => {
+
+    // Pull metadata which contains the demographic information
+    let metadata = data.metadata; 
+
+    // Filter the metadata based on the selected value 
+    let filteredData = metadata.filter(result => result.id == selectedValue); 
+
+    
+  })
+}
+
+
+
+
+
+
+
 
 init(); 
 
